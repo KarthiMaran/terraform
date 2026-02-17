9a11bde12553e8532b42f282cfbd47afa5df1690
KarthiMaran <karthimaran109@gmail.com>
2026-02-17 20:33:03 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull the Nginx image
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+# Run the container
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "nginx"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
